b32186ba2a24dcd8803f0681e2737f169a8c3989
Kane <phoboes@live.com>
2017-03-14 00:47:52 +1100

    Initial.

---
+++ js/scaling.js
@@ -0,0 +1,62 @@
+app = app || {};
+app.winScale = {};
+
+app.winScale.$winHeight;
+app.winScale.$winWidth;
+
+
+
+var setsideScale = function(){
+  app.winScale.$winHeight = window.innerHeight;
+  app.winScale.$winWidth = window.innerWidth;
+  $sideScale.style.height  = app.winScale.$winHeight + 'px';
+  app.winScale.meterLength = parseInt(window.getComputedStyle( document.getElementsByClassName('intMarker')[0] ).height) / 2;
+  $sideScale.style.fontSize = ( parseInt($sideScale.style.height) / 20) + 'px';
+};
+
+var setseascapeScale = function(){
+  app.winScale.$winHeight = window.innerHeight;
+  $seascape.style.height = app.winScale.meterLength * 11050 + "px";
+}
+
+window.onload = function( e ){
+  $seascape = document.getElementById('seascape');
+  $seascapeHeight = $seascape.offsetHeight;
+  $sideScale = document.getElementById( 'sideScale' );
+  setsideScale();
+  setseascapeScale();
+
+
+  app.data.oceanicZones.forEach( function( zone ){
+    var $zone = document.createElement('div');
+    // debugger
+    $zone.className = "zone";
+    $zone.innerHTML = zone.name;
+
+    zonePercentFromTop = ( ( zone.start / ( parseInt( $seascape.style.height ) / app.winScale.meterLength ) ) * 100  ) / 100 * parseInt( $seascape.style.height );
+    console.log( zonePercentFromTop );
+    // debugger
+    $zone.style.marginTop = parseInt( zonePercentFromTop ) + 'px';
+
+
+    $seascape.appendChild( $zone );
+
+    console.log( zone.name )
+    console.log( zone.start )
+
+  } );
+};
+
+
+window.onresize = function(){
+  setsideScale();
+  setseascapeScale();
+}
+
+window.onscroll = function(){
+  var $depthCount = document.getElementById('depthGauge');
+  var depth = Math.round( ( scrollY / app.winScale.meterLength ) * 10 ) / 10 
+  $depthCount.innerHTML = ( depth ) + " m";
+  var pressure = depth * 1.457
+  console.log( (Math.round( pressure * 10 ) / 10).toFixed(2) );
+}
---
+++ scaling.js
@@ -0,0 +1,7 @@
+window.onload = function( e ){
+  $container = document.getElementsByClassName('container');
+  $containerHeight = $container[0].offsetHeight;
+
+
+
+}
