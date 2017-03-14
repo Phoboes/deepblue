6f8c4063da879218f5c4538950a04419d20df6fe
Kane <phoboes@live.com>
2017-03-14 13:16:16 +1100

    Changed css, added data points to JS.

---
+++ js/datapoints.js
@@ -0,0 +1,76 @@
+var app = app || {};
+app.data = {};
+
+app.data.seafloor = 10994;
+
+app.data.oceanicZones = [
+    {
+      name: "Epipelagic zone",
+      aka: "The Sunlight zone",
+      start: 0
+    },
+    {
+    name: "Mesopelagic zone",
+    aka: "The Twilight zone",
+    start: 200
+    },
+    {
+    name: "Bathypelagic zone",
+    aka: "The Midnight zone",
+    start: 1000
+    },
+    {
+    name: "Abyssopelagic zone",
+    aka: "The Abyss",
+    start: 4000
+    },
+    {
+    name: "Hedalpelagic zone",
+    aka: "The Trenches",
+    start: 9000
+    },
+    ,
+    {
+    name: "Challenger deep",
+    aka: "The Trenches",
+    start: 10994
+    }
+  ];
+
+  app.data.humanAchievement = {
+    20: "For every 10 meters you descend, the volume of air in your body is halved. Lungs compress to 1/3 their normal size. Around now, a human with no equipment reaches neutral bouyancy."
+    40: "The depth limit for recreational scuba divers.",
+    102: "The deepest freedive without using fins.",
+    332: "The deepest SCUBA dive ever achieved.",
+    730: "Estimated maximum depth of US war submarines."
+    4000: "The deepest capacity at which Trawlers can operate.",
+    10898: "The depth James Cameron reached in 2012.",
+    10911: "The deepest diving point of the Trieste - the deepest humans have ever been."
+  };
+
+  app.data.biology = {
+    10: "Red light no penetrates no deeper. Blood at this depth looks dark green.",
+    160: "Photosynthesis is no longer a viable survival strategy.",
+    300: "Deepest dive by a Bottlenose dolphin.",
+    990: "Many creatures are starting to depend on 'Marine Snow' (dead things falling in the water column) for food.",
+    2000: "The limits at which Sperm whales can dive.",
+    2100: "The average depth where hydrothermal vents can be found."
+    3000: "The limits of the deepest diving mammal - The Cuvier's beaked whale."
+  };
+
+  app.data.geography = {
+    45: "The deepest points of Sydney harbour - the deepest (and largest) natural harbour on Earth.",
+    3688: "The average depth of all oceans.",
+    8848: "The height of Everest.",
+    10210: "The total height (including underwater) of Mauna Kea. The tallest mountain on Earth."
+  };
+
+  app.data.architecture = {
+    108: "The depth for the final resting place of the Kursk.",,
+    301: "The height of eiffel tower"
+    541: "The One World Trade Center.",
+    828: "The tallest building on earth, the Burj Khalifa.",
+    3810: "The depth for the final resting place of the titanic.",
+    4900: "The deepest successful salvage operation - retrieving the black box of South African Airways Flight 295.",
+    5762: "The deepest known wreck, The Rio Grande."
+  };
