473f53235170ccf381097634de57b779f96d31fc
Kane <Phoboes@live.com>
2017-03-14 16:37:52 +1100

    Hopefully better
    Fixed double renders on elements.

--- js/scaling.js
+++ js/scaling.js
@@ -4,7 +4,21 @@ app.winScale = {};
 app.winScale.$winHeight;
 app.winScale.$winWidth;
 
+var removeElementsByClass = function (className){
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+};
 
+var setCounters = function(){
+  var $depthCount = document.getElementById('depthGauge');
+  var depth = Math.round( ( scrollY / app.winScale.meterLength ) * 10 ) / 10
+  $depthCount.innerHTML = ( depth ) + " m";
+  var pressure = depth * 1.457
+  // console.log( (Math.round( pressure * 10 ) / 10).toFixed(2) );
+  document.getElementById('pressureGauge').innerHTML = (Math.round( pressure * 10 ) / 10).toFixed(2) + " psi"
+}
 
 var setsideScale = function(){
   app.winScale.$winHeight = window.innerHeight;
@@ -20,6 +34,9 @@ var setseascapeScale = function(){
 }
 
 var setFactoids = function(){
+
+  removeElementsByClass( 'factoid' );
+
   for( data in app.data.facts ){
     console.warn( data )
     for( factset in app.data.facts[data] ){
@@ -41,6 +58,9 @@ var setFactoids = function(){
 };
 
 var setZones = function(){
+
+  removeElementsByClass( 'zone' );
+
   app.data.oceanicZones.forEach( function( zone ){
     var $zone = document.createElement('div');
     // debugger
@@ -57,10 +77,13 @@ var setZones = function(){
   } );
 }
 
-
-
-
-
+var setScales = function(){
+  setsideScale();
+  setseascapeScale();
+  setFactoids();
+  setZones();
+  setCounters();
+}
 
 
 
@@ -71,25 +94,14 @@ window.onload = function( e ){
   $seascape = document.getElementById('seascape');
   $seascapeHeight = $seascape.offsetHeight;
   $sideScale = document.getElementById( 'sideScale' );
-  setsideScale();
-  setseascapeScale();
-  setFactoids();
-  setZones();
+  setScales();
 };
 
 
 window.onresize = function(){
-  setsideScale();
-  setseascapeScale();
-  setFactoids();
-  setZones();
+  setScales();
 }
 
 window.onscroll = function(){
-  var $depthCount = document.getElementById('depthGauge');
-  var depth = Math.round( ( scrollY / app.winScale.meterLength ) * 10 ) / 10
-  $depthCount.innerHTML = ( depth ) + " m";
-  var pressure = depth * 1.457
-  // console.log( (Math.round( pressure * 10 ) / 10).toFixed(2) );
-  document.getElementById('pressureGauge').innerHTML = (Math.round( pressure * 10 ) / 10).toFixed(2) + " psi"
+  setScales();
 }
