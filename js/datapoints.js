5d0f05db308bc79a5ff836d9e3d3004cfd7c11c9
Kane <Phoboes@live.com>
2021-03-08 13:53:47 +1100

    Updated spelling & css.

--- js/datapoints.js
+++ js/datapoints.js
@@ -4,98 +4,98 @@ app.data.scrollPoints = [];
 app.data.seafloor = 10994;
 
 app.data.oceanicZones = [
-    {
-      name: "Epipelagic zone",
-      aka: "The Sunlight zone",
-      start: 0
-    },
-    {
-    name: "Mesopelagic zone",
+  {
+    name: "The Epipelagic zone",
+    aka: "The Sunlight zone",
+    start: 0,
+  },
+  {
+    name: "Entering the Mesopelagic zone",
     aka: "The Twilight zone",
-    start: 200
-    },
-    {
-    name: "Bathypelagic zone",
+    start: 200,
+  },
+  {
+    name: "Entering the Bathypelagic zone",
     aka: "The Midnight zone",
-    start: 1000
-    },
-    {
-    name: "Abyssopelagic zone",
+    start: 1000,
+  },
+  {
+    name: "Entering the Abyssopelagic zone",
     aka: "The Abyss",
-    start: 4000
-    },
-    {
-    name: "Hedalpelagic zone",
+    start: 4000,
+  },
+  {
+    name: "Entering the Hedalpelagic zone",
     aka: "The Trenches",
-    start: 9000
-    },
-    ,
-    {
+    start: 9000,
+  },
+  ,
+  {
     name: "Challenger deep",
-    aka: "The Trenches",
-    start: 10994
-    }
-  ];
+    aka: "The deepest known point on earth",
+    start: 10994,
+  },
+];
 
-  app.data.facts = {};
+app.data.facts = {};
 
-  app.data.facts.humanAchievement = {
-    20: "For every 10 meters you descend, the volume of air in your body is halved. <br>Lungs compress to 1/3 their normal size.<br>Around now, a human with no equipment reaches neutral or negative bouyancy.",
-    40: "The depth limit for recreational scuba divers.",
-    102: "The deepest freedive without using fins.",
-    332: "The deepest SCUBA dive ever achieved.",
-    730: "Estimated maximum dive depth of Seawolf class submarines.",
-    // Check me.
-    // 4000: "The deepest capacity at which Trawlers can operate.",
-    10898: "The depth James Cameron reached in 2012.",
-    10911: "The deepest diving point of the Trieste - the deepest humans have ever been."
-  };
+app.data.facts.humanAchievement = {
+  20: "For every 10 meters you descend, the volume of air in your body is halved. <br>Lungs compress to 1/3 their normal size.<br>Around now, a human with no equipment reaches neutral or negative bouyancy.",
+  40: "The depth limit for recreational scuba divers.",
+  102: "The deepest freedive without using fins.",
+  332: "The deepest SCUBA dive ever achieved.",
+  730: "Estimated maximum dive depth of Seawolf class submarines.",
+  // Check me.
+  // 4000: "The deepest capacity at which Trawlers can operate.",
+  10898: "The depth James Cameron reached in 2012.",
+  10911: "The deepest diving point of the Trieste (1960) -- the deepest humans have ever been.",
+};
 
-  app.data.facts.biology = {
-    10: "Red light penetrates no deeper. Blood at this depth looks dark green.",
-    115: "The height of 'Hyperion', The world's tallest redwood tree.",
-    160: "Almost no sunlight penetrates this deep.<br> Photosynthesis is no longer a viable survival strategy.<br/>Bioluminescence is the only new sources of light.",
-    259: "The deepest known dive by an Orca.",
-    // 300: "Deepest recorded dive by a Bottlenose dolphin.",
-    360: "Lantern fish begin to appear. Lantern fish make up up to 65% of the deep sea fish biomass.<br/>The estimated global biomass of lanternfish is 550 - 660 million metric tonnes, several times the entire world fisheries catch.",
-    400: "During World War II, were puzzled by what appeared to be a false sea floor 300–500 metres deep at day, and less deep at night. <br/>This turned out to be sonar bouncing off of the swim bladders of millions of fish staying at depth during the daylight.",
-    506: "The deepest recorded dive of a Blue Whale - The largest animal to have ever existed.",
-    990: "Many creatures are starting to depend on 'Marine Snow' (dead things falling in the water column) for food.<br>Any trace light from the surface is gone.",
-    1200: "The deepest recorded dive by a Great White shark.",
-    2000: "The limits at which Sperm whales can dive.",
-    2100: "The average depth where hydrothermal vents can be found.",
-    2200: "The deepest known range of the Colossal Squid.",
-    2388: "The deepest recorded depth of an Elephant Seal - the deepest diving seal species.",
-    3000: "The limits of the deepest diving mammal - The Cuvier's beaked whale.",
-    3675: "The lowest reaches of the Portugese Dogfish - The deepest-living shark.",
-    4500: "Deep in the bathypelagic zone, the pressures are crushing, and the temperatures are very low.<br/>Almost all fish at this depth have slow metabolisms and hunt purely by ambush. Due to the crushing pressure, most fish at these depths entirely lack swimbladders.",
-    6000: "At this depth, due to pressure, water needs to be nearly 500 degrees celcius to reach boiling point.",
-    7000: "The lowest ranges of the Dumbo Octopus, the deepest dwelling cephalopod.",
-    //revise
-    8143: "The lowest recorded depth of any living fish (a species of Snailfish).",
-    8200: "The deepest theoretical depth at which any fish could survive. Beyond this point, the pressure crushes cells."
-  };
+app.data.facts.biology = {
+  10: "Red light is already barely visible. <br/>Blood at this depth looks dark green. As we go deeper, the last of the red will be filtered out of the visible spectrum. This is why a lot of sea life is red. They appear effectively black at depth.",
+  115: "The height of 'Hyperion', The world's tallest redwood tree.",
+  160: "Almost no sunlight penetrates this deep.<br> Photosynthesis is no longer a viable survival strategy.<br/>Bioluminescence is the only source of light.",
+  259: "The deepest known dive by an Orca.",
+  // 300: "Deepest recorded dive by a Bottlenose dolphin.",
+  360: "Lantern fish begin to appear. Lantern fish make up up to 65% of the deep sea fish biomass.<br/>The estimated global biomass of lanternfish is 550 - 660 million metric tonnes, several times the entire world's annual fisheries catches combined.",
+  400: "During World War II, submariners were puzzled by shifting sea floors. During the day sonar would indicate depths for 300-500  meters, but during the evenings, it appeared to rise.<br/>This turned out to be sonar bouncing off of the swim bladders of millions of fish which, during daylight, keep to the relative safety of the depths, rising to feed under the cover of night.",
+  506: "The deepest recorded dive of a Blue Whale -- The largest animal to have ever existed.",
+  990: "Many creatures are starting to depend on 'Marine Snow', decaying organic matter that filters down from the water column above, for nutrition.<br>Any trace light from the surface is entirely gone.",
+  1200: "The deepest recorded dive by a Great White shark.",
+  2000: "The limits at which Sperm whales can dive.",
+  2100: "The average depth where hydrothermal vents can be found.",
+  2200: "The deepest known range of the Colossal Squid.",
+  2388: "The deepest recorded depth of an Elephant Seal -- the deepest diving seal species.",
+  3000: "The limits of the deepest diving mammal -- The Cuvier's beaked whale.",
+  3675: "The lowest reaches of the Portugese Dogfish -- The deepest-dwelling shark.",
+  4500: "Deep in the bathypelagic zone, the pressures are crushing, and the temperatures are very low.<br/>Almost all fish at this depth have very slow metabolisms and hunt purely by ambush to minimise energy loss. <br/>Due to the crushing pressure, most fish at these depths entirely lack swimbladders.",
+  6000: "At this depth, due to pressure, water needs to be nearly 500 degrees celcius (932°F) to reach boiling point.",
+  7000: "The lowest ranges of the Dumbo Octopus, the deepest dwelling cephalopod.",
+  //revise
+  8143: "The lowest recorded depth of any living fish.",
+  8200: "The deepest theoretical depth at which any fish could survive. Beyond this point, pressure starts to cause issues on a cellular level.",
+};
 
-  app.data.facts.geography = {
-    45: "The deepest points of Sydney harbour - the deepest (and largest) natural harbour on Earth.",
-    1857: "The depth of the Grand Canyon.",
-    3688: "The average depth of all oceans.",
-    5895: "The height of mount Kilamanjaro",
-    6009: "The depth of Yarlung Zangbo, The deepest canyon on earth.",
-    8848: "The height of Everest.",
-    10210: "The total height (including undersea) of Mauna Kea.<br>The tallest mountain on Earth."
-  };
+app.data.facts.geography = {
+  45: "The deepest points of Sydney harbour - the deepest (and largest) natural harbour on Earth.",
+  1857: "The depth of the Grand Canyon.",
+  3688: "The average depth of all oceans.",
+  5895: "The height of mount Kilamanjaro",
+  6009: "The depth of Yarlung Zangbo, The deepest land canyon.",
+  8848: "The height of Everest.",
+  10210: "The total height (including undersea) of Mauna Kea.<br>The tallest mountain on Earth.",
+};
 
-  app.data.facts.architecture = {
-    108: "The final resting depth of the Kursk.",
-    301: "The height of Eiffel tower",
-    541: "The height of the One World Trade Center.",
-    828: "The tallest building on earth, the Burj Khalifa.",
-    2450: "The Perdido, the deepest floating oil rig on Earth.",
-    3810: "The depth at which the Titanic rests.",
-    4900: "The deepest successful salvage operation - retrieving the black box of South African Airways Flight 295.",
-    5762: "The deepest known wreck, The Rio Grande."
-  };
+app.data.facts.architecture = {
+  108: "The final resting depth of the Kursk.",
+  301: "The height of Eiffel tower",
+  541: "The height of the One World Trade Center.",
+  828: "The tallest building on earth, the Burj Khalifa.",
+  2450: "The operating depth of The Perdido, the deepest floating oil rig on Earth.",
+  3810: "The depth at which the Titanic rests.",
+  4900: "The deepest successful salvage operation - retrieving the black box of South African Airways Flight 295.",
+  5762: "The deepest known wreck, The Rio Grande. Just over half way down now.",
+};
 
 // for( data in app.data.facts ){
 //   console.warn( data )
--- js/scaling.js
+++ js/scaling.js
@@ -8,98 +8,116 @@ app.winScale.$winWidth;
 //    Used to kill off duplicate elements on rescale.
 // ----------------------------------------------------
 
-var removeElementsByClass = function (className){
-    var elements = document.getElementsByClassName(className);
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
-    }
+var removeElementsByClass = function (className) {
+  var elements = document.getElementsByClassName(className);
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
 };
 
-var getUniqueValues = function( arr ){
+var getUniqueValues = function (arr) {
   var storedArr = [];
   for (var i = arr.length - 1; i >= 0; i--) {
-    if( storedArr.indexOf( arr[i] ) === -1 ){
-      storedArr.push( arr[i] );
+    if (storedArr.indexOf(arr[i]) === -1) {
+      storedArr.push(arr[i]);
     }
   }
   // console.log( storedArr.sort(function (a, b) {  return a - b;  }) )
-  return storedArr.sort(function (a, b) {  return a - b;  });
+  return storedArr.sort(function (a, b) {
+    return a - b;
+  });
 };
-
 
 // ----------------------------------------------------
 // ------------------ Start setters -------------------
 // Set scales of all elements and distances, based on the user's viewport.
 
-
-var setCounters = function(){
-  var $depthCount = document.getElementById('depthGauge');
+var setCounters = function () {
+  var $depthCount = document.getElementById("depthGauge");
   // Our depth is calculated by distance from the top of our window, divided by the value of our calculated meter (then rounded).
-  var depth = Math.round( ( scrollY / app.winScale.meterLength ) * 10 ) / 10
-  $depthCount.innerHTML = ( depth ) + " m";
+  var depth = Math.round((scrollY / app.winScale.meterLength) * 10) / 10;
+  $depthCount.innerHTML = depth + " m";
   // 14.57 psi increase in pressure, per 10m.
-  var pressure = depth * 1.457
-  document.getElementById('pressureGauge').innerHTML = (Math.round( pressure * 10 ) / 10).toFixed(2) + " psi";
+  var pressure = depth * 1.457;
+  document.getElementById("pressureGauge").innerHTML =
+    (Math.round(pressure * 10) / 10).toFixed(2) + " psi";
 };
 
-var setsideScale = function(){
-  $sideScale = document.getElementById( 'sideScale' );
+var setsideScale = function () {
+  $sideScale = document.getElementById("sideScale");
   app.winScale.$winHeight = window.innerHeight;
   app.winScale.$winWidth = window.innerWidth;
-  $sideScale.style.height  = app.winScale.$winHeight + 'px';
-  app.winScale.meterLength = parseInt(window.getComputedStyle( document.getElementsByClassName('intMarker')[0] ).height) / 2;
-  $sideScale.style.fontSize = ( parseInt($sideScale.style.height) / 20) + 'px';
+  $sideScale.style.height = app.winScale.$winHeight + "px";
+  app.winScale.meterLength =
+    parseInt(
+      window.getComputedStyle(document.getElementsByClassName("intMarker")[0])
+        .height
+    ) / 2;
+  $sideScale.style.fontSize = parseInt($sideScale.style.height) / 20 + "px";
 };
 
-var setseascapeScale = function(){
+var setseascapeScale = function () {
   app.winScale.$winHeight = window.innerHeight;
   // Set the window ocean depth based on our calculated value of a meter.
   $seascape.style.height = app.winScale.meterLength * 11050 + "px";
 };
 
-var setFactoids = function(){
+var setFactoids = function () {
   // Purge any existing factoids, otherwise resizing etc ends up in duplicate elements.
-  removeElementsByClass( 'factoid' );
-  for( data in app.data.facts ){
-    for( factset in app.data.facts[data] ){
-      var $factoid = document.createElement('div');
+  removeElementsByClass("factoid");
+  for (data in app.data.facts) {
+    for (factset in app.data.facts[data]) {
+      var $factoid = document.createElement("div");
       $factoid.className = "factoid data";
       $factoid.innerHTML = app.data.facts[data][factset];
       // Calculate our offset from the top based on the meter value provided to our data points.
-      var zonePercentFromTop = ( ( parseInt( factset ) / ( parseInt( $seascape.style.height ) / app.winScale.meterLength ) ) * 100  ) / 100 * parseInt( $seascape.style.height );
+      var zonePercentFromTop =
+        (((parseInt(factset) /
+          (parseInt($seascape.style.height) / app.winScale.meterLength)) *
+          100) /
+          100) *
+        parseInt($seascape.style.height);
       // console.log( zonePercentFromTop );
-      $factoid.style.marginTop = parseInt( zonePercentFromTop ) + 'px';
-      app.data.scrollPoints.push( zonePercentFromTop );
-      $seascapeGradient.appendChild( $factoid );
+      $factoid.style.marginTop = parseInt(zonePercentFromTop) + "px";
+      app.data.scrollPoints.push(zonePercentFromTop);
+      $seascapeGradient.appendChild($factoid);
     }
   }
 };
 
-var setZones = function(){
-  removeElementsByClass( 'zone' );
-  app.data.oceanicZones.forEach( function( zone ){
-    var $zone = document.createElement('div');
+var setZones = function () {
+  removeElementsByClass("zone");
+  app.data.oceanicZones.forEach(function (zone) {
+    var $zone = document.createElement("div");
 
     $zone.className = "zone data";
-    $zone.innerHTML = zone.name;
-    zonePercentFromTop = ( ( zone.start / ( parseInt( $seascape.style.height ) / app.winScale.meterLength ) ) * 100  ) / 100 * parseInt( $seascape.style.height );
+    $zone.innerHTML = `
+      ${zone.name}
+      <h4>${zone.aka}</h4>
+    `;
+    zonePercentFromTop =
+      (((zone.start /
+        (parseInt($seascape.style.height) / app.winScale.meterLength)) *
+        100) /
+        100) *
+      parseInt($seascape.style.height);
     // console.log( zonePercentFromTop );
 
-    $zone.style.marginTop = parseInt( zonePercentFromTop ) + 'px';
-    app.data.scrollPoints.push( zonePercentFromTop );
-    $seascapeGradient.appendChild( $zone );
-  } );
+    $zone.style.marginTop = parseInt(zonePercentFromTop) + "px";
+    app.data.scrollPoints.push(zonePercentFromTop);
+    $seascapeGradient.appendChild($zone);
+  });
 };
 
 // Mass function calls, to save me having to write out this cluster for every window change event.
 
-var scaleHuman = function(){
-  var $svg = document.getElementById("human")
-  $svg.style.height = app.winScale.meterLength * 2;
-  $svg.style.bottom = 20+"vh";
+var scaleHuman = function () {
+  var $svg = document.getElementById("human");
+  $svg.style.height = app.winScale.meterLength * 2 + "px";
+  $svg.style.bottom = 20 + "vh";
 };
 
-var setScales = function(){
+var setScales = function () {
   app.data.scrollPoints = [];
   setsideScale();
   setseascapeScale();
@@ -107,7 +125,7 @@ var setScales = function(){
   setZones();
   setCounters();
   // debugger
-  app.data.sortedScrollPoints = getUniqueValues( app.data.scrollPoints );
+  app.data.sortedScrollPoints = getUniqueValues(app.data.scrollPoints);
   // setPoints( app.data.sortedScrollPoints );
   // spacePoints();
 };
@@ -141,110 +159,118 @@ var setScales = function(){
 
 // first add raf shim
 // http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
-window.requestAnimFrame = (function(){
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
+window.requestAnimFrame = (function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
 })();
 
 // main function
 function scrollToY(scrollTargetY, speed, easing) {
-    // scrollTargetY: the target scrollY property of the window
-    // speed: time in pixels per second
-    // easing: easing equation to use
+  // scrollTargetY: the target scrollY property of the window
+  // speed: time in pixels per second
+  // easing: easing equation to use
 
-    var scrollY = window.scrollY,
-        scrollTargetY = scrollTargetY || 0,
-        speed = speed || 2000,
-        easing = easing || 'easeOutSine',
-        currentTime = 0;
+  var scrollY = window.scrollY,
+    scrollTargetY = scrollTargetY || 0,
+    speed = speed || 2000,
+    easing = easing || "easeOutSine",
+    currentTime = 0;
 
-    // min time .1, max time .8 seconds
-    var time = Math.max(.1, Math.min(Math.abs(scrollY - scrollTargetY) / speed, .8));
+  // min time .1, max time .8 seconds
+  var time = Math.max(
+    0.1,
+    Math.min(Math.abs(scrollY - scrollTargetY) / speed, 0.8)
+  );
 
-    // easing equations from https://github.com/danro/easing-js/blob/master/easing.js
-    var PI_D2 = Math.PI / 2,
-        easingEquations = {
-            easeOutSine: function (pos) {
-                return Math.sin(pos * (Math.PI / 2));
-            },
-            easeInOutSine: function (pos) {
-                return (-0.5 * (Math.cos(Math.PI * pos) - 1));
-            },
-            easeInOutQuint: function (pos) {
-                if ((pos /= 0.5) < 1) {
-                    return 0.5 * Math.pow(pos, 5);
-                }
-                return 0.5 * (Math.pow((pos - 2), 5) + 2);
-            }
-        };
-
-    // add animation loop
-    function tick() {
-        currentTime += 1 / 60;
-
-        var p = currentTime / time;
-        var t = easingEquations[easing](p);
-
-        if (p < 1) {
-            requestAnimFrame(tick);
-
-            window.scrollTo(0, scrollY + ((scrollTargetY - scrollY) * t));
-        } else {
-            console.log('scroll done');
-            window.scrollTo(0, scrollTargetY);
+  // easing equations from https://github.com/danro/easing-js/blob/master/easing.js
+  var PI_D2 = Math.PI / 2,
+    easingEquations = {
+      easeOutSine: function (pos) {
+        return Math.sin(pos * (Math.PI / 2));
+      },
+      easeInOutSine: function (pos) {
+        return -0.5 * (Math.cos(Math.PI * pos) - 1);
+      },
+      easeInOutQuint: function (pos) {
+        if ((pos /= 0.5) < 1) {
+          return 0.5 * Math.pow(pos, 5);
         }
-    }
+        return 0.5 * (Math.pow(pos - 2, 5) + 2);
+      },
+    };
 
-    // call it once to get started
-    tick();
+  // add animation loop
+  function tick() {
+    currentTime += 1 / 60;
+
+    var p = currentTime / time;
+    var t = easingEquations[easing](p);
+
+    if (p < 1) {
+      requestAnimFrame(tick);
+
+      window.scrollTo(0, scrollY + (scrollTargetY - scrollY) * t);
+    } else {
+      console.log("scroll done");
+      window.scrollTo(0, scrollTargetY);
+    }
+  }
+
+  // call it once to get started
+  tick();
 }
 
 // scroll
-scrollToY(0, 1500, 'easeInOutQuint');
+scrollToY(0, 1500, "easeInOutQuint");
 
-var clickNav = function(){
+var clickNav = function () {
   windowPos = window.scrollY;
   var dataPointLength = app.data.sortedScrollPoints.length;
   var targetPoint = 0;
-  if( this.id === 'up' ){
+  if (this.id === "up") {
     for (var i = dataPointLength - 1; i >= 0; i--) {
       // If we're sitting between data points, navigate to the nearest point with a lower Y index and stop the loop.
-      var roundedPos = Math.round( app.data.sortedScrollPoints[i] );
-      if( windowPos > roundedPos ){
+      var roundedPos = Math.round(app.data.sortedScrollPoints[i]);
+      if (windowPos > roundedPos) {
         targetPoint = roundedPos;
         // window.scrollTo( 0, targetPoint );
-        scrollToY( roundedPos );
+        scrollToY(roundedPos);
         break;
         // If we're already on a point, navigate to the one before.
-      } else if ( windowPos === Math.round( app.data.sortedScrollPoints[i] ) ){
-        targetPoint = app.data.sortedScrollPoints[ i - 1 ];
+      } else if (windowPos === Math.round(app.data.sortedScrollPoints[i])) {
+        targetPoint = app.data.sortedScrollPoints[i - 1];
         // window.scroll( 0, targetPoint );
         // controller.scrollTo( { y: targetPoint } );
-        scrollToY( targetPoint );
+        scrollToY(targetPoint);
         break;
       }
     }
     // console.log('up')
     // scrollRamp( windowPos, targetPoint );
   } else {
-    for ( var i = 0; i <= dataPointLength - 1; i++ ) {
+    for (var i = 0; i <= dataPointLength - 1; i++) {
       // console.log(i)
-      var roundedPos = Math.round( app.data.sortedScrollPoints[i] );
+      var roundedPos = Math.round(app.data.sortedScrollPoints[i]);
       // debugger
-      if( windowPos < Math.round( app.data.sortedScrollPoints[i] ) ){
+      if (windowPos < Math.round(app.data.sortedScrollPoints[i])) {
         // debugger
         // window.scrollTo( 0, roundedPos );
-        scrollToY( roundedPos );
+        scrollToY(roundedPos);
         break;
-      } else if ( windowPos === Math.round( app.data.sortedScrollPoints[i] ) && app.data.sortedScrollPoints[i] < app.data.sortedScrollPoints[i].length ){
+      } else if (
+        windowPos === Math.round(app.data.sortedScrollPoints[i]) &&
+        app.data.sortedScrollPoints[i] < app.data.sortedScrollPoints[i].length
+      ) {
         // debugger
-        targetPoint = app.data.sortedScrollPoints[ i + 1 ];
+        targetPoint = app.data.sortedScrollPoints[i + 1];
         // window.scrollTo( 0, targetPoint );
-        controller.scrollTo( { y: targetPoint } );
+        controller.scrollTo({ y: targetPoint });
         break;
       }
     }
@@ -255,93 +281,76 @@ var clickNav = function(){
 // ----------------------------------------------------
 var currentData;
 
-var highlight = function( pos ){
-  for( var i = 0; app.data.scrollPoints.length > i; i++ ){
-    var range = 30;
+var highlight = function (pos) {
+  for (var i = 0; app.data.scrollPoints.length > i; i++) {
+    var range = 100;
 
-    if( pos <= app.data.scrollPoints[i] + range && pos >= app.data.scrollPoints[i] - range ){
+    if (
+      pos <= app.data.scrollPoints[i] + range &&
+      pos >= app.data.scrollPoints[i] - range
+    ) {
       currentData = document.getElementsByClassName("data")[i];
       currentData.style.color = "rgba(220, 220, 220, 0.8)";
       return;
     } else {
       // debugger
-      if( currentData.style.color === "rgba(220, 220, 220, 0.8)" ){
+      if (currentData.style.color === "rgba(220, 220, 220, 0.8)") {
         currentData.style.color = "rgba(220, 220, 220, 0.2)";
       }
     }
   }
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var fadeSplashPage = function(){
-  var $splashDiv = document.getElementsByClassName('whitespaceLander')[0];
+var fadeSplashPage = function () {
+  var $splashDiv = document.getElementsByClassName("whitespaceLander")[0];
 
   var $svg = document.getElementById("human");
   var $svgWrap = document.getElementsByClassName("splashSVGWrap")[0];
-  var $svgText = document.querySelector('#scaling g');
-  var $svgScale = document.querySelector('#scaling');
+  var $svgText = document.querySelector("#scaling g");
+  var $svgScale = document.querySelector("#scaling");
 
-  var $triggerButton = document.getElementById("splashButton")
-  $triggerButton.parentNode.removeChild( $triggerButton );
+  var $triggerButton = document.getElementById("splashButton");
+  $triggerButton.parentNode.removeChild($triggerButton);
 
-  $svgScale.classList.add('removeScale');
-  $svgText.classList.add('removeScaleText');
-  $splashDiv.classList.add('triggerFade');
-  setTimeout( function(){
+  $svgScale.classList.add("removeScale");
+  $svgText.classList.add("removeScaleText");
+  $splashDiv.classList.add("triggerFade");
+  setTimeout(function () {
     // var elem = document.getElementsByClassName(".container");
-    $splashDiv.parentNode.removeChild( $splashDiv );
+    $splashDiv.parentNode.removeChild($splashDiv);
     $svg.style.transition = "bottom 0s";
-  }, 3000 );
-
+  }, 3000);
 
   // Default our highlighted state to our first data point.
   currentData = document.getElementsByClassName("data")[0];
-  highlight( window.scrollY );
-
+  highlight(window.scrollY);
 
   scaleHuman();
-    // $svg.style.top = window.innerHeight - document.getElementById("human").clientHeight;
+  // $svg.style.top = window.innerHeight - document.getElementById("human").clientHeight;
 };
 
-
-window.onload = function( e ){
-  highlight( window.scrollY );
-  $seascape = document.getElementById('seascape');
-  $seascapeGradient = document.getElementById('darkGradient');
+window.onload = function (e) {
+  highlight(window.scrollY);
+  $seascape = document.getElementById("seascape");
+  $seascapeGradient = document.getElementById("darkGradient");
   setScales();
 
-var $arrows = document.getElementsByClassName('arrow');
+  var $arrows = document.getElementsByClassName("arrow");
   for (var i = $arrows.length - 1; i >= 0; i--) {
     $arrows[i].onclick = clickNav;
   }
 
-
-  var $splashButton = document.getElementById('splashButton');
+  var $splashButton = document.getElementById("splashButton");
   $splashButton.onclick = fadeSplashPage;
 };
 
-window.onresize = function(){
+window.onresize = function () {
   setScales();
   scaleHuman();
 };
 
-window.onscroll = function(e){
+window.onscroll = function (e) {
   // debugger
   setCounters();
-  highlight( window.scrollY );
-
+  highlight(window.scrollY);
 };
